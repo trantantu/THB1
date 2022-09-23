975493085fad3b37cf78afb13a341eb3a259dfe6
trantantu <trantantu85@gmail.com>
2022-09-23 10:03:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/routes/contact.route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const contact = require("../controllers/contact.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(contact.findALL)
+    .post(contact.create)
+    .delete(contact.deleteALL);
+
+router.route("/favorite")
+    .get(contact.findALLFavorite);
+
+router.route("/id")
+    .get(contact.findOne)
+    .put(contact.update)
+    .delete(contact.delete);
+
+module.exports = router;
